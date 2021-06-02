e9c3b6f4f8388b7a3f735bc13652b22dba35626b
Hoang Nguyen <hoang@mission.plus>
2021-06-02 13:16:46 +0700

    write base Cypress

--- cypress/integration/button.spec.js
+++ cypress/integration/button.spec.js
@@ -6,14 +6,16 @@ describe('Button', function () {
     cy.visit('/')
     this.button = new Button();
     this.button.getComponentButton().click()
-    cy.fixture('base-button.json').as('data')
+    cy.fixture('base-button').then(function (data) {
+      this.data = data;
+    })
   })
 
   it('compare base button style', function () {
     this.button.getBaseButton().compareStyles(this.data.base_button);
   });
 
-  it('compare base button style', function () {
+  it('compare base button style when disabled', function () {
     cy.get('#disabled').click();
     this.button.getBaseButton().should("have.class", "bp3-disabled")
     this.button.getBaseButton().compareStyles(this.data.disabled_base_button);
